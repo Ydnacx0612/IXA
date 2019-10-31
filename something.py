41977dabc2b04e7a78664675f151bfbe63e18a1d
Tin Nguyen <TNguyen4191@student.pps.net>
2019-10-31 11:13:55 -0700

    What ever

---
+++ something.py
@@ -0,0 +1,18 @@
+from madison_axi.axi import *
+
+
+def spiky_circle():
+    initialize()
+
+    move_to(100, 0)
+    point_in_direction(260)
+    pen_down()
+
+    for i in range(50):
+        move_forward(127)
+        turn_right(127)
+
+        cleanup()
+
+if __name__ == "__main__":
+    spiky_circle()
